77dfaf6eef31f1b4d9b80591b7165f9191a0b383
junior027 <128190824+junior027@users.noreply.github.com>
2023-05-10 18:48:07 -0300

    Add files via upload

---
+++ junior-workspace/WinAppFirst/principal.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WinAppFirst
+{
+    partial class principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNomeCompleto = new System.Windows.Forms.Label();
+            this.txtNomeCompleto = new System.Windows.Forms.TextBox();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNomeCompleto
+            // 
+            this.lblNomeCompleto.AutoSize = true;
+            this.lblNomeCompleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeCompleto.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblNomeCompleto.Location = new System.Drawing.Point(37, 19);
+            this.lblNomeCompleto.Name = "lblNomeCompleto";
+            this.lblNomeCompleto.Size = new System.Drawing.Size(118, 20);
+            this.lblNomeCompleto.TabIndex = 0;
+            this.lblNomeCompleto.Text = "nome completo";
+            // 
+            // txtNomeCompleto
+            // 
+            this.txtNomeCompleto.Font = new System.Drawing.Font("Elephant", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeCompleto.Location = new System.Drawing.Point(30, 43);
+            this.txtNomeCompleto.Name = "txtNomeCompleto";
+            this.txtNomeCompleto.Size = new System.Drawing.Size(674, 24);
+            this.txtNomeCompleto.TabIndex = 1;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(30, 197);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnExecutar.TabIndex = 2;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            this.btnExecutar.MouseHover += new System.EventHandler(this.btnExecutar_MouseHover);
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdade.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblIdade.Location = new System.Drawing.Point(37, 115);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(50, 20);
+            this.lblIdade.TabIndex = 3;
+            this.lblIdade.Text = "Idade";
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Font = new System.Drawing.Font("Elephant", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdade.Location = new System.Drawing.Point(30, 138);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(674, 24);
+            this.txtIdade.TabIndex = 4;
+            // 
+            // principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(770, 450);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.lblIdade);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.txtNomeCompleto);
+            this.Controls.Add(this.lblNomeCompleto);
+            this.Name = "principal";
+            this.Text = "principal";
+            this.Load += new System.EventHandler(this.principal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNomeCompleto;
+        private System.Windows.Forms.TextBox txtNomeCompleto;
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Label lblIdade;
+        private System.Windows.Forms.TextBox txtIdade;
+    }
+}
---
+++ junior-workspace/WinAppFirst/principal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAppFirst
+{
+    public partial class principal : Form
+    {
+        public principal()
+        {
+            InitializeComponent();
+        }
+
+        private void principal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExecutar_MouseHover(object sender, EventArgs e)
+        {
+            if (txtNomeCompleto.Text == "")
+            {
+                MessageBox.Show("digite um nome");
+            }
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            if(txtNomeCompleto.Text != "")
+            {
+               if(txtIdade.Text != "")
+                {
+                    var idade = int.Parse(txtIdade.Text);
+                    if(idade >= 18)
+                    {
+                        MessageBox.Show("Opa, " + txtNomeCompleto.Text + " voce é maior de idade");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Opa, " + txtNomeCompleto.Text + " voce é menor de idade");
+                    }
+                }
+            }
+        }
+    }
+}
